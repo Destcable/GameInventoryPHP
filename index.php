10d84656833db6def2a865ea07d6eaf5b35c18d0
Destcable <artmobpavlov21@gmail.com>
2023-09-01 10:30:44 +0300

    [UPD]

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+require 'vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 use Destcable\InventoryPHP\Item;
 use Destcable\InventoryPHP\Character;
@@ -7,10 +12,10 @@ use Destcable\InventoryPHP\Inventory;
 $item1 = new Item(1, "Меч", "Мощный меч для атаки", "оружие", ["атака" => 10]);
 $item2 = new Item(2, "Щит", "Прочный щит для защиты", "броня", ["защита" => 5]);
 
-$inventory = new Inventory($character);
-$character = new Character(1, "Герой", $inventory);
+// $inventory = new Inventory($character);
+// $character = new Character(1, "Герой", $inventory);
 
-$character->pickUpItem($item1);
-$character->pickUpItem($item2);
+// $character->pickUpItem($item1);
+// $character->pickUpItem($item2);
 
-$character->dropItem($item1);
+// $character->dropItem($item1);
